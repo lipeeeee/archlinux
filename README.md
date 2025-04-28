d7e340abdd198e89675f397c2476a1d2ae43ae8c
lipeeeee <filipe.pereirafga@gmail.com>
2025-04-28 18:58:52 +0100

    manager

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # archlinux dotfiles
 archlinux dotfiles and auto-installer/updater
+
+# TODO
+make github actions archlinux jobs that runs manager with all flags
--- manager.py
+++ manager.py
@@ -16,6 +16,7 @@ __doc__ = """
 # 5. IF PATH IS IMPORTANT MAKE SURE TO VERIFY(FOR EXAMPLE MUST BE IN $HOME/archlinux)
 # 7. IMPORTANT! PRESENTS SUMMARY BEFORE EXECUTING TASKS AND ASKS Y/N TO CONTINUE
 # 8. -200 lines
+# 9. ignored_modules as arg? -y as do it anyway flag?
 
 # - os.path.realpath(os.curdir) -> gives current script path
 # - os.fwalk(path) -> gives a list of dirs and files of path
@@ -25,10 +26,11 @@ __doc__ = """
 # and that would make use of the summary AND get rid of the summary at the same time
 
 # BACKUP = if backup flag passed, a backup should be done before & after runtime
-# Backup saves should be saved like 'BEFORE/AFTER-MANAGER:date'
+# Backup saves should be saved like 'PRE/POST-MANAGER:$date'
 # however if its a new instance and timeshift is not installed, it should just ignore
 
-import sys
+import sys, os
+import copy
 import logging
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(funcName)s:%(lineno)s - %(message)s')
 
@@ -36,9 +38,11 @@ from argparse import ArgumentParser
 from enum import Enum
 
 # globals
-ignored_modules = []
+ignored_modules = ["nvim"]
 parser = ArgumentParser(prog="ArchManagerPY")
 logger = logging.getLogger(__name__)
+managerpy_directory = os.path.realpath(os.curdir)
+managerpy_modules_path = os.path.join(managerpy_directory, "modules/")
 
 # Class to handle script args
 class ScriptParams:
@@ -70,24 +74,44 @@ def parse_args() -> ScriptParams:
 
     return ScriptParams(actions, args.force, args.backup)
 
+# Override of List.__subt__
+def list_subt_override(list1: list, list2: list) -> list:
+    flist = copy.copy(list1)
+    for x in flist:
+        if list2.__contains__(x): flist.remove(x)
+    return flist
+
 def ask_yes_no(prompt: str) -> bool:
     return input(prompt + " (y/n): ").strip().lower() in ("y", "yes")
 
 if __name__ == "__main__":
     logger.info("Initializing ArchManagerPY...")
+
+    # Modules directory gen
+    assert os.path.isdir(managerpy_modules_path), f"Could not find modules folder in {managerpy_directory}"
+    modules_gen = os.fwalk(managerpy_modules_path)
+
+    # Script params
     try:
         sp = parse_args()
     except Exception as e:
         logger.error(e)
         parser.print_help()
         sys.exit(1)
+
+    found_modules:list[str] = modules_gen.__next__()[1]
+    affected_modules:list[str] = list_subt_override(found_modules, ignored_modules)
     
     # TODO: get all modules and then list everything (with `ignored_modules`) and ask_yes_no
-    logger.info("------ Parsed Arguments Summary " + "-"*50)
-    logger.info(f"Actions received: {sp.actions}")
-    logger.info(f"Ignoring errors ? {sp.ignore_errors}")
-    logger.info(f"Backing up      ? {sp.backup}")
-    logger.info("-"*(32+50))
-
-
+    logger.info("------ Parsed Arguments Summary " + "-"*32)
+    logger.info(f"Actions received\t: {sp.actions}")
+    logger.info(f"Ignoring errors\t? {sp.ignore_errors}")
+    logger.info(f"Backing up     \t? {sp.backup}")
+    logger.info("------ Modules Summary " + "-"*(64-23))
+    logger.info(f"Found modules\t: {found_modules}")
+    logger.info(f"Ignored modules\t: {ignored_modules}")
+    logger.info(f"Will be affected\t: {affected_modules}")
+    logger.info("-"*(32+32))
+    if not ask_yes_no("Do you agree with the shown information?"):
+        sys.exit(0)
 
