020aae490b18e7d6ac83b73985bc7cb5bfda717e
lipeeeee <filipe.pereirafga@gmail.com>
2025-05-01 04:18:26 +0100

    fixed nvim update script

--- modules/nvim/update.sh
+++ modules/nvim/update.sh
@@ -10,24 +10,21 @@ update() {
   # To update inplace nvim we basically do the same thing as install and override last install
 
   # Download latest appimage
-  APPIMAGE_URL=$(curl -sL "$API" \
-    | jq -r '
-      .assets[]
-      | select(.name == "nvim-linux-x86_64.appimage")
-      | .browser_download_url')
-  curl -L "$APPIMAGE_URL" -o "$TMPDIR/nvim.appimage"
+  API="https://api.github.com/repos/neovim/neovim/releases/latest"
+  APPIMAGE_URL=$(curl -sL "$API" | jq -r '.assets[] | select(.name == "nvim-linux-x86_64.appimage") | .browser_download_url')
+  curl -L "$APPIMAGE_URL" -o "$TMP_DIR/nvim.appimage"
 
   # Extract it and deploy to /
-  chmod +x "$TMPDIR/nvim.appimage"
+  chmod +x "$TMP_DIR/nvim.appimage"
   (cd "$TMP_DIR" && ./nvim.appimage --appimage-extract)
 
-  $DST_DIR = "/squashfs-root"
+  DST_DIR="/squashfs-root"
   if [[ -e $DST_DIR || -L $DST_DIR ]]; then
     echo "Removing existing $DST_DIR"
     rm -rf "$DST_DIR"
   fi
 
-  sudo rsync -a "$TMPDIR/squashfs-root/" /
+  sudo rsync -a "$TMP_DIR/squashfs-root/" /
 }
 
 # 3. Execute all
